02ce1ea2e25b2477f14c28a5bb11df142b5f4c97
Kavya Vijayan <100032344+KavyaKukku@users.noreply.github.com>
2024-06-13 15:34:00 +0530

    Update set_of_lines.sh

--- set_of_lines.sh
+++ set_of_lines.sh
@@ -1,37 +1,41 @@
 ---------file operation-----
-read a text file in shell script
+read 2 text file in shell script
 
 thtx.txt
 How are you
 I am Fine.
 Thank You
 
+thtx2.txt
+Something went wrong!
+please check
 ---code------
 
-read -p "Enter your file" first
-echo "Enter the line no"
-read n1
-echo "Enter the 2nd line no"
-read n2
-shift
+if [ $# -le 2 ]
+then
+echo "Syntax is $0 1 2 thtx.txt thtx2.txt"
+exit 1
+fi
+n1=$1
+n2=$2
 n3=`expr $n2 - $n1 + 1`
-while read line
+shift
+shift
+while [ $# -ne 0 ]
 do
-echo "lines from $n1 to $n2 from $1\n"
+echo "lines from $n1 to $n2 from $1 lines from $n1 to $n2 from $2 \n"
 head -$n2 $1 | tail -$n3
 shift
-exit
-done<$first
+done
 
 
 ----output-----
 
-[mca2325@Server shell]$ bash extract_lines.sh
-Enter your file: thtx.txt
-Enter the line no
-1
-Enter the 2nd line no
-2
-lines from 1 to 2 from \n
+[mca2325@Server shell]$ vi extract_lines.sh
+[mca2325@Server shell]$ bash extract_lines.sh 1 2 thtx.txt thtx2.txt
+lines from 1 to 2 from thtx.txt lines from 1 to 2 from thtx2.txt \n
+How are you
 I am Fine.
-Thank You
+lines from 1 to 2 from thtx2.txt lines from 1 to 2 from  \n
+Something went wrong!
+Please check
